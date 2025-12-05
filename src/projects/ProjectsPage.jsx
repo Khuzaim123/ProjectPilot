9d657d4b9ca816999108cf96d1e30547419cb30e
Khuzaim Adnan Qureshi <khuzaimadnana@gmail.com>
2025-12-05 22:11:25 +0500

    Frontend Completed

---
+++ src/projects/ProjectsPage.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
+
+function ProjectsPage() {
+  const [projects, setProjects] = useState(MOCK_PROJECTS);
+
+  const saveProject = (project) => {
+    let updatedProjects = projects.map((p) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
+  };
+
+  return (
+    <>
+      <h1>Projects</h1>
+      <ProjectList onSave={saveProject} projects={projects} />
+    </>
+  );
+}
+
+export default ProjectsPage;
